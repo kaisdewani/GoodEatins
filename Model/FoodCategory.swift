5d87f67997b47d3136e70923aab2597e6b9d7044
Kais Dewani <kaisdewani@yahoo.com>
2019-04-21 18:11:16 -0400

    add data set and images for project

---
+++ Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by Kais on 4/21/19.
+//  Copyright © 2019 Kais. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by Kais on 4/21/19.
+//  Copyright © 2019 Kais. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
