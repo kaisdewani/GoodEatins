eb9682177284bf3294afd31e67b4961e301542be
Kais Dewani <kaisdewani@yahoo.com>
2019-04-21 20:20:01 -0400

    each individual cell with the image and text

--- View/CategoryCell.swift
+++ View/CategoryCell.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class CategoryCell: UITableViewCell {
-
+    
+    @IBOutlet weak var categoryImg: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        categoryImg.layer.cornerRadius = 10
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func configureCell(category: FoodCategory) {
+        categoryImg.image = UIImage(named: category.imageName)
+        categoryTitle.text = category.title
     }
+    
+    
+
 
 }
