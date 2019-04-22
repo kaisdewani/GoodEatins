1ea83e7f0aa190558f075959eba8164f0093fb43
Kais Dewani <kaisdewani@yahoo.com>
2019-04-21 20:46:07 -0400

    add variable that will obtain the data from HomeVC

--- Controller/RecipesSelectionVC.swift
+++ Controller/RecipesSelectionVC.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class RecipesSelectionVC: UIViewController {
+    
+    var selectedCategory: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,15 +18,6 @@ class RecipesSelectionVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
